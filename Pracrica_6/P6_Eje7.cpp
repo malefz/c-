ca5d0634d105ba966d210c26f803595df8f64d4a
Malena <maleeiris789@gmail.com>
2024-11-07 14:21:35 -0300

    Delete Pracrica_6 directory

--- Pracrica_6/P6_Eje7.cpp
+++
@@ -1,5 +0,0 @@
-/*
-Palíndromo
-Escribe un programa que determine si una cadena es un palíndromo (se lee igual de adelante hacia atrás y viceversa).
-    -  Pista: Usa el método .substr() y .length() para comparar partes de la cadena.
-*/
